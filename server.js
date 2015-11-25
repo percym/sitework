84909ed283829197498bffa0f25af85ca2ed45a1
percy mugadza <percym@localhost.localdomain>
2015-11-25 09:49:42 +0200

    more port changes

--- server.js
+++ server.js
@@ -45,12 +45,12 @@ app.get('*',function(req,res){
                  });
 
 
-                 app.listen(config.port, function(err){
+                 app.listen(process.env.PORT || 3000, function(err){
 
                      if(err){
                      console.log(err);
                      }else{
-                     console.log('Listening on port' + config.port);
+                     console.log('Listening on port' + process.env.Port);
                      }
                  });
 //some comment
