39af837283eb99eaabfa073301b82ecf167e0cda
percy mugadza <percym@localhost.localdomain>
2015-11-25 08:45:07 +0200

    changed port from 8080 t0 5000 on server.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
     module.exports= {
         "database":"mongodb://percym:77drslo1@ds039850.mongolab.com:39850/devicedb",
-           "port":process.env.port || 8080,
+           "port":process.env.port || 5000,
                 "secretKey":"vakomanandofanenode"
                     }
